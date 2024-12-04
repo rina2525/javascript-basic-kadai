6acd2e810fe1f134f7d2fa10e00ee4dd5e8836c5
rina2525 <rina.yasuno@icloud.com>
2024-12-05 00:51:12 +0900

    課題をアップロード

---
+++ kadai_020/event.js
@@ -0,0 +1,18 @@
+const clickBtn = document.getElementById('btn');
+const list =document.createElement('text');
+
+
+clickBtn.addEventListener('click',() =>{
+  const addText = document.createElement('h2');
+  addText.textContent = 'ボタンをクリックしました';
+  list.appendChild(addText);
+  text.textContent = 'ボタンをクリックしました。';
+});
+
+
+  
+
+
+
+
+
