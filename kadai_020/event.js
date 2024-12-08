f7aaa931f1180917cf329c580fe5d72a6751fba2
rina2525 <rina.yasuno@icloud.com>
2024-12-08 17:12:54 +0900

    ボタンクリック時の結果を変更

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -2,17 +2,6 @@ const clickBtn = document.getElementById('btn');
 const list =document.createElement('text');
 
 
-clickBtn.addEventListener('click',() =>{
-  const addText = document.createElement('h2');
-  addText.textContent = 'ボタンをクリックしました';
-  list.appendChild(addText);
-  text.textContent = 'ボタンをクリックしました。';
+btn.addEventListener('click',() =>{
+  text.textContent = 'ボタンをクリックしました';  
 });
-
-
-  
-
-
-
-
-
